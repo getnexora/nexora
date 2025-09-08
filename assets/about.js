f4468302abc96b3eff5835056fbe7c8df4cc199d
Steven Thompson <steven.thompson.ne@gmail.com>
2025-09-08 04:19:19 -0500

    Update about.js

--- assets/about.js
+++ assets/about.js
@@ -1,1 +1,13 @@
+// assets/js/about.js
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("About page loaded");
+
+  // Simple fade-in for content
+  const sections = document.querySelectorAll("section");
+  sections.forEach((sec, i) => {
+    sec.style.opacity = 0;
+    sec.style.transition = "opacity 1s ease";
+    setTimeout(() => (sec.style.opacity = 1), 300 * i);
+  });
+});
 
